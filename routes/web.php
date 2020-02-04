332f334da76a97691a810f213ee6ec6d087e7a5b
Takaki <takaki55730317@gmail.com>
2020-02-04 15:20:01 +0900

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// archivesディレクトリに行ってindex.blade.phpを読み込む
+Route::get('/archives/', function() {
+    return view('archives.index');
+});
+
+// archivesディレクトリに行ってarchivesのcategory.blade.phpを読み込み$categoryのparamsを読む
+Route::get('/archives/{category}/', function($category) {
+    return view('archives.category', ['category'=>$category]);
+});
+
+Route::post('/join/', function() {
+    return '入会申し込み完了';
+});
+
+Route::get('/join/', function() {
+    return redirect()->to('/');
+});
+
+Route::get('/{id}/', function($id) {
+    return "{$id}のページ";
+});
